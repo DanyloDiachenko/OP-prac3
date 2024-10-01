c74acd7530bf1c541becef9b4aab80a2ec178bb4
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-10-01 17:12:21 +0300

    start

--- index.c
+++ index.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
+#include <math.h>
+
+double computeRoot(const double value, const int exponent, const double accuracy) {
+    double y = value;
+
+    do {
+        const double sigma = (1.0 / exponent) * (value / pow(y, exponent - 1) - y);
+
+        if (fabs(sigma) < accuracy) {
+            return y;
+        }
+
+        y = y + sigma;
+    } while (1);
+}
 
 int main() {
-    printf("start");
+    double sqrtValue;
+    int exponent;
+    double accuracy;
+
+    printf("Enter the value of sqrtValue: ");
+    scanf("%lf", &sqrtValue);
+
+    printf("Enter the value of exponent: ");
+    scanf("%d", &exponent);
+
+    printf("Enter the accuracy (e.g., 0.0001): ");
+    scanf("%lf", &accuracy);
+
+    const double result = computeRoot(sqrtValue, exponent, accuracy);
+
+    printf("The %d-th root of %.2f is approximately: %.5f\n", exponent, sqrtValue, result);
 
     return 0;
 }
