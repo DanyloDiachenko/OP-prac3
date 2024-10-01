9950af8e02335397a988212e28198858d4ebf25e
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-10-01 17:40:05 +0300

    FIRST OPTION

--- index.c
+++ index.c
@@ -4,6 +4,10 @@
 double computeRoot(const double value, const int exponent, const double accuracy) {
     double y = value;
 
+    if (exponent < 0) {
+        return 1.0 / pow(value, -exponent);
+    }
+
     do {
         const double sigma = (1.0 / exponent) * (value / pow(y, exponent - 1) - y);
 
@@ -20,18 +24,43 @@ int main() {
     int exponent;
     double accuracy;
 
-    printf("Enter the value of sqrtValue: ");
-    scanf("%lf", &sqrtValue);
-
     printf("Enter the value of exponent: ");
     scanf("%d", &exponent);
+
+    if (exponent == 0) {
+        printf("Exponent cannot be equal to 0\n");
+        return -1;
+    }
+
+    if (exponent > 0) {
+        printf("Enter the value of sqrtValue: ");
+        scanf("%lf", &sqrtValue);
+
+        if (sqrtValue < 0 && exponent % 2 == 0) {
+            printf("sqrtValue cannot be less than 0 for even exponents\n");
+            return -1;
+        }
+    } else {
+        printf("Enter the value of sqrtValue: ");
+        scanf("%lf", &sqrtValue);
+
+        if (exponent < 0 && sqrtValue <= 0 && exponent % 2 == 0) {
+            printf("sqrtValue must be greater than 0 for even negative exponents\n");
+            return -1;
+        }
+
+        if (sqrtValue == 0) {
+            printf("sqrtValue cannot be 0 for negative exponents\n");
+            return -1;
+        }
+    }
 
     printf("Enter the accuracy (e.g., 0.0001): ");
     scanf("%lf", &accuracy);
 
     const double result = computeRoot(sqrtValue, exponent, accuracy);
 
-    printf("The %d-th root of %.2f is approximately: %.5f\n", exponent, sqrtValue, result);
+    printf("The %d-th root of %.2f is approximately: %.7f\n", exponent, sqrtValue, result);
 
     return 0;
 }
