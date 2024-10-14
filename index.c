5c00e837eb3211d9108930931bb5e911c196f2ed
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-10-14 20:37:22 +0300

    GLOBAL CODE FIXES

--- index.c
+++ index.c
@@ -1,139 +1,152 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdbool.h>
 
 #define EPSILON 1e-15
+#define MAX_ROOT 1e+10
+#define MIN_ROOT -1e+10
+#define MAX_EXPONENT 1000
+#define MIN_EXPONENT -1000
+#define MAX_ACCURACY 1e-1
+#define MIN_ACCURACY 1e-15
 
 int getDecimalPlaces(double accuracy) {
-    int decimalPlaces = 0;
-    while (accuracy < 1 && decimalPlaces < 15) {
-        accuracy *= 10;
-        decimalPlaces++;
-    }
-    return decimalPlaces;
+	int decimalPlaces = 0;
+
+	while (accuracy < 1 && decimalPlaces < 15) {
+		accuracy *= 10;
+		decimalPlaces++;
+	}
+
+	return decimalPlaces;
 }
 
-double calculateRoot(const double value, const int exponent, const double accuracy) {
-    if (exponent == 0) {
-        printf("Exponent cannot be zero.\n");
-        return NAN;
-    }
+double calculateRoot(const double root, const int exponent, const double accuracy) {
+	if (exponent < 0) {
+		const double positiveExponent = -exponent;
+		const double positiveRoot = calculateRoot(root, positiveExponent, accuracy);
 
-    if (exponent < 0) {
-        if (value == 0) {
-            printf("Cannot compute root for value 0 with negative exponent.\n");
-            return NAN;
-        }
-        double positiveExponent = -exponent;
-        double positiveRoot = calculateRoot(value, positiveExponent, accuracy);
-        if (positiveRoot == 0) {
-            printf("Division by zero encountered.\n");
-            return NAN;
-        }
-        return 1.0 / positiveRoot;
-    }
+		return 1.0 / positiveRoot;
+	}
 
-    double y = value;
-    double delta;
+	double result = root;
+	double delta;
 
-    do {
-        if (pow(y, exponent - 1) == 0) {
-            printf("Division by zero encountered during iteration.\n");
-            return NAN;
-        }
-        delta = (1.0 / exponent) * (value / pow(y, exponent - 1) - y);
-        y = y + delta;
-    } while (fabs(delta) >= accuracy);
+	do {
+		delta = (1.0 / (double) exponent) * (root / pow(result, (double) exponent - 1.0) - result);
+		result = result + delta;
+	} while (fabs(delta) >= accuracy);
 
-    return y;
+	return result;
 }
 
 int main() {
-    double root;
-    int exponent;
-    double accuracy;
+	double root, accuracy;
+	int exponent;
+	bool inputValidity = false;
 
-    do {
-        printf("Enter the value of exponent: ");
-        if (scanf("%d", &exponent) != 1) {
-            printf("Invalid input. Please enter an integer.\n");
-            fflush(stdin);
-            exponent = 0;
-            continue;
-        }
-        fflush(stdin);
+	do {
+		printf("Enter the value of exponent: ");
+		if (scanf("%d", &exponent) != 1) {
+			printf("Invalid input. Please enter an integer.\n");
+			fflush(stdin);
+			exponent = 0;
 
-        if(exponent == 0) {
-            printf("Exponent cannot be equal to 0\n");
-        }
-    } while(exponent == 0);
+			continue;
+		}
+		fflush(stdin);
 
-    if (exponent > 0) {
-        printf("Enter the value of root: ");
-        if (scanf("%lf", &root) != 1) {
-            printf("Invalid input for root.\n");
-            fflush(stdin);
-            return -1;
-        }
-        fflush(stdin);
+		if (exponent == 0) {
+			printf("Exponent cannot be equal to 0\n");
+		}
 
-        if (root < 0 && exponent % 2 == 0) {
-            printf("Root value cannot be less than 0 for even exponents\n");
-            return -1;
-        }
-    } else {
-        printf("Enter the value of root: ");
-        if (scanf("%lf", &root) != 1) {
-            printf("Invalid input for root.\n");
-            fflush(stdin);
-            return -1;
-        }
-        fflush(stdin);
+		if (exponent > MAX_EXPONENT || exponent < MIN_EXPONENT) {
+			printf("Exponent value is too large or too small. Please enter a value between %d and %d.\n", MIN_EXPONENT, MAX_EXPONENT);
+			exponent = 0;
+		}
 
-        if (root <= 0 && (-exponent) % 2 == 0) {
-            printf("Root value must be greater than 0 for even negative exponents\n");
-            return -1;
-        }
+	} while (exponent == 0);
 
-        if (root == 0) {
-            printf("Root value cannot be 0 for negative exponents\n");
-            return -1;
-        }
-    }
+	do {
+		printf("Enter the value of root: ");
+		if (scanf("%lf", &root) != 1) {
+			printf("Invalid input for root. Please enter a valid number.\n");
+			fflush(stdin);
 
-    do {
-        printf("Enter the accuracy (e.g., 0.0001): ");
-        if (scanf("%lf", &accuracy) != 1 || accuracy <= 0) {
-            printf("Invalid input for accuracy. Please enter a positive number and not zero.\n");
-            fflush(stdin);
-            accuracy = 0;
-            continue;
-        }
-        fflush(stdin);
+			continue;
+		}
+		fflush(stdin);
 
-        double logarithmValue = log10(accuracy);
-        double roundedLogarithm = round(logarithmValue);
+		if (root > MAX_ROOT || root < MIN_ROOT) {
+			printf("Root value is too large or too small. Please enter a value between %.0lf and %.0lf.\n", MIN_ROOT, MAX_ROOT);
 
-        if (fabs(logarithmValue - roundedLogarithm) > EPSILON) {
-            printf("Accuracy should be a power of 10 (e.g., 1, 0.1, 0.01, etc.)\n");
-            accuracy = 0;
-            continue;
-        }
+			continue;
+		}
 
-        if (accuracy < 1e-15) {
-            printf("Accuracy cannot be more precise than 1e-15.\n");
-            accuracy = 0;
-        }
-    } while(accuracy == 0);
+		if (exponent > 0) {
+			if (root < 0 && exponent % 2 == 0) {
+				printf("Root value cannot be less than 0 for even exponents\n");
 
-    const int decimalPlaces = getDecimalPlaces(accuracy);
+				continue;
+			}
+		} else {
+			if (root <= 0 && (-exponent) % 2 == 0) {
+				printf("Root value must be greater than 0 for even negative exponents\n");
 
-    const double result = calculateRoot(root, exponent, accuracy);
+				continue;
+			}
 
-    if (!isnan(result)) {
-        printf("The %d-th root of %.lf is approximately: %.*lf\n", exponent, root, decimalPlaces, result);
-    } else {
-        printf("Failed to compute the root due to invalid input or computation error.\n");
-    }
+			if (root == 0) {
+				printf("Root value cannot be 0 for negative exponents\n");
 
-    return 0;
+				continue;
+			}
+		}
+
+		inputValidity = 1;
+
+	} while (!inputValidity);
+
+	if (root == 0) {
+		printf("The %d-th root of 0 is 0\n", exponent);
+
+		return 0;
+	}
+
+	do {
+		printf("Enter the accuracy (e.g., 0.0001): ");
+		if (scanf("%lf", &accuracy) != 1 || accuracy <= 0) {
+			printf("Invalid input for accuracy. Please enter a positive number and not zero.\n");
+			fflush(stdin);
+			accuracy = 0;
+
+			continue;
+		}
+		fflush(stdin);
+
+		if (accuracy < MIN_ACCURACY || accuracy > MAX_ACCURACY) {
+			printf("Accuracy value is out of range. Please enter a value between %.1e and %.1e.\n", MIN_ACCURACY, MAX_ACCURACY);
+			accuracy = 0;
+		}
+
+		const double logarithmValue = log10(accuracy);
+		const double roundedLogarithm = round(logarithmValue);
+
+		if (fabs(logarithmValue - roundedLogarithm) > EPSILON) {
+			printf("Accuracy should be a power of 10 (e.g., 1, 0.1, 0.01, etc.)\n");
+			accuracy = 0;
+		}
+
+	} while (accuracy == 0);
+
+	const double result = calculateRoot(root, exponent, accuracy);
+	const int decimalPlaces = getDecimalPlaces(accuracy);
+
+	if (!isnan(result)) {
+		printf("The %d-th root of %.lf is approximately: %.*lf\n", exponent, root, decimalPlaces, result);
+	} else {
+		printf("Failed to compute the root due to invalid input or computation error.\n");
+	}
+
+	return 0;
 }
